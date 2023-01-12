16ba50f5a42a2a65e3f2ecd77f1a593e48e34ef0
kriipa <kripathapa2002@gmail.com>
2023-01-12 09:13:59 +0545

    half stact notes app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 // import logo from './logo.svg';
 // import './App.css';
 import { useState } from "react";
-import Feedback from "./compotents/feedback";
+// import Feedback from "./compotents/feedback";
 
-function App() {
 
-  const [counter, setCounter] = useState(0)
+function App(props) {
 
-  // setTimeout(
-  //   () => setCounter(counter +1),1000
-  // )
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] =  useState(true)
 
-  const handleClick = () => setCounter(counter + 1)
-  // function handleClick(){
-  //   return setCounter(counter +1)
-  // }
+  const notesToShow = showAll 
+  ? notes
+  : notes.filter(n => n.important === true)
 
-  const handleReset = () =>  setCounter(0)
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
-  console.log(`rendering ${counter}`)
+  const handleAdd = (event) => {
+    event.preventDefault()
+    console.log(event.target)
+
+    //create a note object
+    const note = {
+      id: notes.length + 1,
+      content: newNote,
+      date : new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+
+    if(newNote !== '') setNotes(notes.concat(note))
+    setNewNote('')
+  }
 
   return (
     <div>
-      {/* <h1> Hellow {name} <br></br>
-      {age} years already <br></br>
-      beautiful beautiful loki
-      </h1> */}
-      <h2> {counter} </h2>
-      <button onClick={handleClick}>
-        Plus +
+      <h2> NOtes </h2>
+      <button onClick={() => setShowAll(!showAll)}>
+        toggle
       </button>
-      <button onClick={handleReset}>
-        Reset 
-      </button>
-
-      <Feedback/>
+      <ul> 
+        {notesToShow.map( note => 
+          <li key={note.id}> 
+            <p>{note.content}</p> 
+            <p> {note.date}</p> 
+          </li>
+        )}
+      </ul>
+      <form> 
+        <input value = {newNote} onChange = {handleChange}/> 
+          <button onClick={handleAdd}> add </button> 
+      </form>
     </div>
   );
-
 }
 
 export default App;
-
